17d86d28c6ff8bebe4078001a74d12e493545393
Douglas Fernando da Silva <doug.fernando@gmail.com>
2009-06-11 15:36:21 -0300

    first commit, the scripts set lyrics on tracks using LyricsWiki

---
+++ itunes_helper.rb
@@ -0,0 +1,55 @@
+require 'win32ole'
+require 'soap/wsdlDriver' 
+require 'iconv'
+
+class ITunesHelper
+  def initialize(lyrics_service)
+    @itunes = WIN32OLE.new('iTunes.Application')
+    @tracks = @itunes.LibraryPlaylist.Tracks
+    @lyrics_service = lyrics_service
+  end
+
+  def show_all_tracks
+    @tracks.each do |track|
+      print track.Name() + "\n"
+    end
+  end
+
+  def set_all_lyrics
+    puts "Number of tracks to be analyzed: #{@tracks.count}"
+    index = 1
+    @tracks.each do |track|
+      if track.lyrics.empty?
+        lyrics = @lyrics_service.get_lyrics(track.Artist(), track.Name())
+
+        if not lyrics.empty?
+          track.lyrics = @lyrics_service.get_lyrics(track.Artist(), track.Name())
+          self.print_track(index, @tracks.count, track, "lyrics WAS found and set")
+        else
+          self.print_track(index, @tracks.count, track, "lyrics WAS NOT found")
+        end
+      else
+        self.print_track(index, @tracks.count, track, "already had lyrics")
+      end
+      index = index + 1
+    end
+  end
+
+  def print_track(index, total, track, result) 
+    puts "Track #{index}/#{total}: #{track.Artist()} - #{track.Name()}: #{result}"
+  end
+end
+
+class LyricsServiceProxy
+  def initialize
+    @driver = SOAP::WSDLDriverFactory.new("http://lyricwiki.org/server.php?wsdl").create_rpc_driver
+    puts "Lyrics web service initialized"
+  end
+
+  def get_lyrics(artist, track_name)
+    Iconv.iconv("LATIN1", "UTF-8", @driver.getSong(artist, track_name).lyrics).to_s
+  end
+end
+
+itunesH = ITunesHelper.new(LyricsServiceProxy.new)
+itunesH.set_all_lyrics
